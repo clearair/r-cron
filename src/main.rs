6ff599cc72d4cc8f10a7a295c89049e27ecb8899
naoki <quzhi552@gmail.com>
2025-01-12 01:17:13 +0800

    feat: 优化任务调度和关闭流程
    - 修改配置文件中的 cron 表达式，将任务执行间隔从 10 分钟改为 3 分钟
    - 实现了更优雅的关闭流程，等待所有任务完成后才退出
    - 添加了任务计数功能，实时跟踪剩余任务数量
    - 优化了日志输出，增加了任务开始和完成的日志信息
    - 改进了错误处理，增加了对任务执行失败的警告日志

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use anyhow::Result;
 use chrono::Local;
-use tokio::{fs::File, process::Command, sync::Mutex};
+use tokio::{fs::File, process::Command, sync::{Mutex, Notify}};
 use tokio_cron_scheduler::{JobScheduler, Job};
 use serde::{Serialize, Deserialize};
-use std::{fs::File as StdFile, process, str::FromStr, sync::Arc, time::Duration};
+use std::{fs::File as StdFile, process, str::FromStr, sync::{atomic::{AtomicUsize, Ordering}, Arc}, time::Duration};
 use tokio::signal::unix::{signal, SignalKind}; // 用于捕获信号
 use log::{debug, info, warn};
 use env_logger::{Builder, Env};
 use std::io::{Write};
 use clap::Parser;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -32,7 +31,7 @@ struct Comm {
     cron: String,
 }
 
-// static CONFIG_STR: &'static str = include_str!("../config.yaml"); 
+static CONFIG_STR: &'static str = include_str!("../config.yaml"); 
 
 async fn run() -> anyhow::Result<()> {
     Builder::from_env(Env::default().default_filter_or("info"))
@@ -42,31 +41,47 @@ async fn run() -> anyhow::Result<()> {
         })
         .init();
 
-    // Parse command-line arguments
-    let args = Args::parse();
-    let file = StdFile::open(args.config_file)?;
-    let config: Conf = serde_yaml::from_reader(file)?;
+    // Parse config
+    let config: Conf = serde_yaml::from_str(CONFIG_STR)?;
 
     let sched = Arc::new(Mutex::new(JobScheduler::new().await?));
+    let is_shutdown = Arc::new(Mutex::new(false)); // 标记是否接收到关闭信号
+    // let notify = Arc::new(Notify::new()); // 用于通知任务完成
+
+    let remaining_tasks = Arc::new(AtomicUsize::new(0)); // 用于跟踪剩余任务数量
 
     let tz: chrono_tz::Tz = chrono_tz::Tz::from_str(&config.timezone)?;
 
+    // Add jobs to the scheduler
     for comm in config.comms {
         let cron = comm.cron.clone();
+        let remaining_tasks = remaining_tasks.clone();
+        // let notify = notify.clone();
+        let is_shutdown = is_shutdown.clone();
 
         sched.lock().await.add(
             Job::new_async_tz(cron, tz, move |_uuid, mut _l| {
                 let command = comm.command.clone();
                 let args = comm.args.clone();
+                let remaining_tasks = remaining_tasks.clone();
+                // let notify = notify.clone();
+                let is_shutdown = is_shutdown.clone();
 
                 Box::pin(async move {
-                    info!("执行任务: {} {}", command, args);
+                    let shutdown_guard = is_shutdown.lock().await;
+                    if *shutdown_guard {
+                        info!("收到关闭信号，正在退出...");
+                        return;
+                    }
 
+                    remaining_tasks.fetch_add(1, Ordering::SeqCst); // 增加剩余任务计数
+
+                    info!("执行任务: {} {}", command, args);
                     let output: Result<std::process::Output, std::io::Error> = Command::new(command)
                         .args(args.split(' ').collect::<Vec<_>>())
                         .output()
                         .await;
-
+                    info!("执行命令: {}", remaining_tasks.load(Ordering::SeqCst));
                     if let Ok(output) = output {
                         info!("{}", String::from_utf8_lossy(&output.stdout));
                         if !output.status.success() {
@@ -75,11 +90,29 @@ async fn run() -> anyhow::Result<()> {
                     } else {
                         warn!("错误信息: {}", output.unwrap_err());
                     }
+                    info!("执行命令: {}", remaining_tasks.load(Ordering::SeqCst));
+
+                    // 任务完成后，减少任务计数
+                    remaining_tasks.fetch_sub(1, Ordering::SeqCst);
+
+                    // info!("任务完成，剩余任务数量: {}", remaining_tasks.load(Ordering::SeqCst));
+
+                    // let shutdown_guard = is_shutdown.lock().await;
+            
+                    // info!("信号: {}", *shutdown_guard);
+
+                    // 如果所有任务都完成了，通知主线程
+                    // if remaining_tasks.load(Ordering::SeqCst) == 0 {
+                    //     notify.notify_one();
+                    // }
+
+                    info!("执行完成: {}", remaining_tasks.load(Ordering::SeqCst));
                 })
             })?
         ).await?;
     }
 
+    // 捕获退出信号
     let mut shutdown_signal = signal(SignalKind::interrupt())?;
 
     let sched_clone = Arc::clone(&sched);
@@ -87,21 +120,47 @@ async fn run() -> anyhow::Result<()> {
         sched_clone.lock().await.start().await.unwrap();
     });
 
+    // 等待关闭信号或任务完成
     tokio::select! {
         _ = shutdown_signal.recv() => {
-            info!("正在退出...");
+            info!("收到关闭信号，正在退出...");
+            let mut shutdown_guard = is_shutdown.lock().await;
+            *shutdown_guard = true;
+
+            info!("正在等待任务完成...{}", remaining_tasks.load(Ordering::SeqCst));
+            // 等待所有任务完成
+
+            // 持续监控 remaining_tasks，直到它变为 0
+            while remaining_tasks.load(Ordering::SeqCst) > 0 {
+                info!("等待任务完成... 剩余任务: {}", remaining_tasks.load(Ordering::SeqCst));
+                // 等待一段时间后再次检查 remaining_tasks
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
+            // 停止调度器
             sched.lock().await.shutdown().await?;
+
         }
+
+        // _ = notify.notified() => {
+        //     // 确保所有任务完成后再继续
+        //     let shutdown_guard = is_shutdown.lock().await;
+        //     if *shutdown_guard {
+        //         info!("所有任务已完成.");
+        //         sched.lock().await.shutdown().await?;
+        //     }
+            
+        // }
     }
+
+    // 终止 r-cron 进程
     terminate_rcron_process().await?;
     sched_handle.await.unwrap();
 
     info!("退出完成.");
-
     Ok(())
 }
 
-// todo 不清楚为什么存在一个 r-cron 进程 所以退出时候 强制清除 r-cron 进程
+// 清除 `r-cron` 进程
 async fn terminate_rcron_process() -> Result<(), std::io::Error> {
     let current_pid = process::id();  // 获取当前进程的PID
 
@@ -126,6 +185,7 @@ async fn terminate_rcron_process() -> Result<(), std::io::Error> {
     }
     Ok(())
 }
+
 fn main() {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
@@ -133,4 +193,3 @@ fn main() {
         .unwrap()
         .block_on(run());
 }
-
